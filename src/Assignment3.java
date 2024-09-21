9f385417033cfec90311f904d9164693c86eb0c6
Elijah A. Tucker <etucker210@users.noreply.github.com>
2024-09-21 09:08:29 -0400

    Created Assignment3.java and set up in IntelliJ

---
+++ src/Assignment3.java
@@ -0,0 +1,23 @@
+/*
+ * File Name: Assignment3.java
+ * Author:    Elijah Tucker
+ * Last Edit: 21 Sep 2024
+ *
+ * Prompt: Create a program that accepts a student's name and their grades for two subjects.
+ *         The program should then determine whether the student has passed or failed based
+ *         on the following simple criteria:
+ *         - Pass: The student must have a grade of 50 or above in both subjects.
+ *         - Fail: The student fails if they score below 50 in either of the two subjects.
+ *         More details on the prompt can be found in README.md
+ *
+ * Note:   This project is controlled by VCS so no change log here
+ *
+ */
+
+public class Assignment3 {
+
+    public static void main(String[] args) {
+
+    }
+
+}
